d6e049e3e26ac757dae424f250e8bcdc184064a6
WorseNut <giovannivilarinho76@gmail.com>
2024-08-24 00:13:28 -0300

    atualização

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,14 +1,13 @@
-
 const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
-acordeonTriggers.forEach((trigger) => {
+acordeonTriggers.forEach((trigger) =>{
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement
         const isOpen = acordeon.classList.contains('open')
 
-        if (isOpen) {
+        if (isOpen){
             acordeon.classList.remove('open')
-        } else {
+        } else{
             acordeon.classList.add('open')
         }
     })
